dcaa7d68af2af840f30ae4d8b0f72547f80bd92f
Esteban FM <esteban.fsma@gmail.com>
2024-05-25 14:15:16 -0600

    login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,11 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import clientPromise from "@../../../libs/mongodb"
 
 export const authOptions = {
   // Configure one or more authentication providers
+  adapter: MongoDBAdapter (clientPromise),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
